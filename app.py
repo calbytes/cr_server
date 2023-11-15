1469f8c612bcc166c0f39b31ebc473c07c9db743
calbytes <caalla3@gmail.com>
2023-11-14 20:30:36 -0500

    added wsgi.py entry point

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def get_quote():
     return 'Hello, Flask!\n'
 
+@app.route('/post_contact_message')
+def post_contact_message():
+    return 'message posted!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
