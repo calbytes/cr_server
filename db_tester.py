bde5cb768b11daff9e09d2aacbce69352cbcc909
calbytes <caalla3@gmail.com>
2024-06-26 20:04:32 -0400

    adding test file

---
+++ db_tester.py
@@ -0,0 +1,13 @@
+import db_manager.db_icon_math as dbim
+
+
+
+def test_db():
+    id = 1
+    data = (id,)
+    keywords = dbim.get_keywords_by_content_id(data)
+    print(str(keywords))
+
+
+if __name__ == '__main__':
+    test_db()
